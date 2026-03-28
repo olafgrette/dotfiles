100038f3aa3ced755b12ef26e37781186f98d3b7
Olaf Grette <olaf@grette.org>
2026-03-28 11:19:19 -0700

    Handle hx/helix name difference across platforms

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,14 @@
 fish_add_path ~/.local/bin
 
-set -x EDITOR hx
+if command -q hx
+    set -x EDITOR hx
+    if not command -q helix
+        alias helix hx
+    end
+else if command -q helix
+    set -x EDITOR helix
+    alias hx helix
+end
 
 
 starship init fish | source
