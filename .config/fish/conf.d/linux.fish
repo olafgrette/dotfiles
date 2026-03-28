f656468b04965208f8ab7fcae2b95f7c118ff505
Olaf Grette <olaf@grette.org>
2026-03-28 12:20:40 -0700

    Switch to systemd ssh-agent socket, add blinking bar cursor in tmux

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -10,22 +10,6 @@ alias monitor-desk "cp ~/.config/hypr/monitors.conf.desk ~/.config/hypr/monitors
 alias monitor-tv "cp ~/.config/hypr/monitors.conf.tv ~/.config/hypr/monitors.conf && hyprctl reload"
 
 if not set -q SSH_CONNECTION
-    # ssh-agent: reuse existing agent or start a new one
-    set -l _ssh_env ~/.ssh/environment-$hostname
-    if test -f $_ssh_env
-        source $_ssh_env > /dev/null
-        if not test -S "$SSH_AUTH_SOCK"
-            ssh-agent | grep -v '^echo' > $_ssh_env
-            chmod 600 $_ssh_env
-            source $_ssh_env > /dev/null
-        end
-    else
-        ssh-agent | grep -v '^echo' > $_ssh_env
-        chmod 600 $_ssh_env
-        source $_ssh_env > /dev/null
-    end
-    ssh-add -q 2>/dev/null
-
     # Auto-start Hyprland on tty1
     if not set -q DISPLAY; and test "$XDG_VTNR" = 1
         mkdir -p ~/.cache
